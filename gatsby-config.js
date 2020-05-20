7e579049988e0965b27d79962ce42585929d0ba6
Sebastian Uddén <sebastianudden@sebastians-mbp.lan>
2020-05-20 13:37:27 +0200

    feat: add plugin-manifest and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `simply-flash-cards`,
+        short_name: `flash-cards`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#2b9bcb`,
+        display: `standalone`,
+        icon: `src/images/flash-cards.svg`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
